b76db9a902766fa89bb78e05b2a69ea862954e5f
Paige Power <paige.power2@gmail.com>
2018-02-24 23:04:57 -0500

    icons fully functional now

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     // this refers to app
     // grabbing 
     this.state = {
+      timezone: "",
       temperature: "",
       summary: "",
       icons: ""
@@ -31,64 +32,39 @@ class App extends React.Component {
     .then((res) => {
       console.log(res)
       console.log(res.data.currently.temperature)
+      console.log(res.data.currently.icon)
 
       this.setState ({
+        timezone: res.data.timezone,
         temperature: res.data.currently.temperature,
         summary: res.data.currently.summary,
         icon: res.data.currently.icon
       });
       // skycons
-      // var icons = new Skycons({
-      //   'color':"#000000"
-      // }),
-      //   list = [
-      //     "clear-day", "clear-night", "partly-cloudy-day",
-      //     "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
-      //     "fog"
-      //   ],
-      //   i;
+      let icons = new Skycons({
+        'color':"#000000"
+      }),
+        list = [
+          "clear-day", "clear-night", "partly-cloudy-day",
+          "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+          "fog"
+        ],
+        i;
 
-      //   for (i = list.length; i--;)
-      //     icons.set(list[i], list[i]);
+        for (i = list.length; i--;)
+          icons.set(list[i], list[i]);
 
-      //   icons.play();
-      var icons = new Skycons({ 
-        "color": "lightblue" 
-      });
-
-      icons.set("clear-day", Skycons.CLEAR_DAY);
-      icons.set("clear-night", Skycons.CLEAR_NIGHT);
-      icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
-      icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
-      icons.set("cloudy", Skycons.CLOUDY);
-      icons.set("rain", Skycons.RAIN);
-      icons.set("sleet", Skycons.SLEET);
-      icons.set("snow", Skycons.SNOW);
-      icons.set("wind", Skycons.WIND);
-      icons.set("fog", Skycons.FOG);
-
-      icons.play();
+        icons.play();
     });
   }
 
     render() {
       return (
         <div>
-          <p>Temperature: {this.state.temperature}</p>
-          <p>Summary: {this.state.summary}</p>
-          <canvas id="clear-day" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="clear-night" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="partly-cloudy-night" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="cloudy" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="rain" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="sleet" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="snow" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="wind" width="64" height="64">{this.state.icon}</canvas>
-          <canvas id="fog" width="64" height="64">{this.state.icon}</canvas>
-          {/* <form action="">
-            <input type="text"/>
-            <label htmlFor=""></label>
-          </form> */}
+          <p>{this.state.timezone}</p>
+          <p>{this.state.temperature}℃</p>
+          <p>{this.state.summary}</p>
+          <canvas id={this.state.icon} width="80" height="80"></canvas>
         </div>
       )
     }
