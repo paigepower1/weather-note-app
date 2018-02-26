3c87ed85216ef0b10435c3558e0af2fe3f3127d0
Paige Power <paige.power2@gmail.com>
2018-02-25 22:05:29 -0500

    user input and firebase working

---
+++ dev/scripts/EggCounter.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// EggCounter component
+// put in components folder later
+// const EggCounter = ((props) => {
+//      return(
+//       <li className={props.data}></li>
+//     )
+// }); 
+    
+    
+    
+    
+   
--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,8 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Qs from 'qs';
+import index from 'axios';
+// import EggCounter from './EggCounter';
 // api key: c17bad791422b42a39a8f8e4299b6c53
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB7TVAtpvGhLbdNAdktABZQailxPr58R-I",
+  authDomain: "chicken-app.firebaseapp.com",
+  databaseURL: "https://chicken-app.firebaseio.com",
+  projectId: "chicken-app",
+  storageBucket: "",
+  messagingSenderId: "18953960665"
+};
+firebase.initializeApp(config);
+
+// main App component
+// set initial state in here 
+// and bind functions
 class App extends React.Component {
   constructor() {
     super();
@@ -13,12 +29,30 @@ class App extends React.Component {
       timezone: "",
       temperature: "",
       summary: "",
-      icons: ""
-    }
+      icons: "",
+      // array
+      eggInput: "",
+      eggsLaid: []
+    };
+
+    // bind here
+    this.handleChange = this.handleChange.bind(this);
+    this.addEgg = this.addEgg.bind(this);
+
+  }
+
+  handleChange(e) {
+    console.log("handling the change");
+    console.log(e.target.id);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   }
 
   // here is anything we want to happen after the component renders
   componentDidMount() {
+    // const dbref = firebase.database().ref("/eggsLaid");
+
     axios({
       url: "https://proxy.hackeryou.com",
       params: {
@@ -28,6 +62,7 @@ class App extends React.Component {
         return Qs.stringify(params, { arrayFormat: 'brackets' })
       } 
     })
+    
     // promise
     .then((res) => {
       console.log(res)
@@ -38,8 +73,10 @@ class App extends React.Component {
         timezone: res.data.timezone,
         temperature: res.data.currently.temperature,
         summary: res.data.currently.summary,
-        icon: res.data.currently.icon
+        icon: res.data.currently.icon,
+        // eggInput: ""
       });
+
       // skycons
       let icons = new Skycons({
         'color':"#000000"
@@ -56,8 +93,41 @@ class App extends React.Component {
 
         icons.play();
     });
+
+    const dbref = firebase.database().ref("/eggsLaid");
+
+    dbref.on("value", (snapshot) => {
+      const data = snapshot.val();
+      const state = [];
+      for(let key in data) {
+      data[key].key = key;
+      state.push(data[key]);
+      }
+      console.log(state);
+      this.setState({
+        eggsLaid: state
+      })
+    });
   }
 
+  // user input
+  addEgg(e) {
+    e.preventDefault(); 
+    const eggInput = {
+      value: this.state.eggInput
+    };
+
+    // .ref is method on firebase that makes reference to where we push data to
+    // push data to firebase to eggs array here
+    const dbref = firebase.database().ref("/eggsLaid");
+    dbref.push(eggInput);
+
+    this.setState({
+      eggInput: ""
+    });
+  }
+
+    // everything that gets displayed on page goes here
     render() {
       return (
         <div>
@@ -65,12 +135,26 @@ class App extends React.Component {
           <p>{this.state.temperature}℃</p>
           <p>{this.state.summary}</p>
           <canvas id={this.state.icon} width="80" height="80"></canvas>
+          <form onSubmit={this.addEgg}>
+            <label htmlFor="eggInput">Enter number of eggs laid</label>
+            <input type="text" value={this.state.eggInput} onChange={this.handleChange} id="eggInput"/>
+            <input type="submit" value="Upload to calendar" />
+          </form>
+          {/* <div>
+            {this.state.eggsLaid.map((eggInput) => {
+              return (
+                 <EggCounter data={eggInput} key={eggInput.key}
+              )
+            })}
+          </div> */}
         </div>
       )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// pseudo code
 
 // weather API:
 // using axios make get request and pass URI argument
